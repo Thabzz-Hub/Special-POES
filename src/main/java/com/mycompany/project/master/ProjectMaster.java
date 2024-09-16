3665a6ddcaada4f5845f22326402a54a409393ce
tlesi <tlesi@ThabzzHub>
2024-09-16 09:59:38 +0200

    releasing and updating my code

--- src/main/java/com/mycompany/project/master/ProjectMaster.java
+++ src/main/java/com/mycompany/project/master/ProjectMaster.java
@@ -4,8 +4,7 @@
 
 package com.mycompany.project.master;
 import java.util.Scanner;
-  import java.util.ArrayList;
-import java.util.List;
+
 
 
 /**
@@ -13,101 +12,9 @@ import java.util.List;
  * @author tlesi
  */
 public class ProjectMaster {
-static class User {
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-
-    public User(String username, String password, String firstName, String lastName) {
-        this.username = username;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    // Getters
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
-    public String getFirstName() { return firstName; }
-    public String getLastName() { return lastName; }
-}
-
-static class Login {
-    private List<User> users;
-
-    public Login() {
-        this.users = new ArrayList<>();
-    }
-
-    public boolean checkUserName(String username) {
-        return username.contains("_") && username.length() <= 5;
-    }
-
-    public boolean checkPasswordComplexity(String password) {
-        boolean hasUpperCase = false;
-        boolean hasLowerCase = false;
-        boolean hasNumber = false;
-        boolean hasSpecialChar = false;
-
-        if (password.length() >= 8) {
-            for (char c : password.toCharArray()) {
-                if (Character.isUpperCase(c)) {
-                    hasUpperCase = true;
-                } else if (Character.isLowerCase(c)) {
-                    hasLowerCase = true;
-                } else if (Character.isDigit(c)) {
-                    hasNumber = true;
-                } else {
-                    hasSpecialChar = true;
-                }
-            }
-        }
-
-        return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar;
-    }
-
-    public String registerUser(String username, String password, String firstName, String lastName) {
-        if (!checkUserName(username)) {
-            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.";
-        } else if (!checkPasswordComplexity(password)) {
-            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
-        } else {
-            users.add(new User(username, password, firstName, lastName));
-            return "User registered successfully!";
-        }
-    }
-
-    public boolean loginUser(String username, String password) {
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public String returnLoginStatus(boolean loginSuccess, String username) {
-        if (loginSuccess) {
-            User user = getUserByUsername(username);
-            return "Welcome " + user.getFirstName() + " " + user.getLastName() + ", it is great to see you again.";
-        } else {
-            return "Username or password incorrect, please try again.";
-        }
-    }
-
-    private User getUserByUsername(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
-}
-
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Login login = new Login();
 
@@ -163,6 +70,3 @@ static class Login {
         System.out.println(loginMessage);
     }
 }
-
-   
-
